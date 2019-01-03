7ca2cdf33aefb1fdd718112a0ca1a3145a0e6d69
kakimoso <k.i.moso9532@gmail.com>
2019-01-03 20:28:33 +0900

    turbolinks対応

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,12 +1,11 @@
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
+import TurbolinksAdapter from 'vue-turbolinks'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   const el = document.body.appendChild(document.createElement('hello'))
   const app = new Vue({
     el,
     render: h => h(App)
   })
-
-  console.log(app)
 })
